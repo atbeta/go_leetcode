d1429c9f7f7b0976da7a8acd4204d3a5fa76b1a5
Beta <me@pbeta.me>
2020-08-11 00:48:10 +0800

    add p70 climbing stairs

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+  "fmt"
   "github.com/atbeta/go_leetcode/problems"
-  . "github.com/atbeta/go_leetcode/structs"
 )
 
 func main() {
-  s := []int{1,2,2,1}
-  head := NewListNode(s)
+  //s := []int{1,2,2,1}
+  //head := NewListNode(s)
   //problems.SwapPairs(head)
   //reversed := problems.ReverseList(head)
   //fmt.Println("------------------")
   //reversed.Print()
   // p203
-  removed := problems.RemoveElements(head, 2)
-  removed.Print()
+  //removed := problems.RemoveElements(head, 2)
+  //removed.Print()
+  steps := problems.ClimbStairs(45)
+  fmt.Println(steps)
 }
